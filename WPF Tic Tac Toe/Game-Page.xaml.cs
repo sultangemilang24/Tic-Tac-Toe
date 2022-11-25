2c6582cf247ef22abf26ab84f0827744a3ce94cb
Sultan Gemilang Kemadi <sultangemilang24@students.amikom.ac.id>
2022-11-25 08:43:53 +0700

    First Init
    File dari Rar yang ada di group

---
+++ WPF Tic Tac Toe/Game-Page.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Tic_Tac_Toe
+{
+    /// <summary>
+    /// Interaction logic for Game_Page.xaml
+    /// </summary>
+    public partial class Game_Page : Window
+    {
+        public Game_Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
